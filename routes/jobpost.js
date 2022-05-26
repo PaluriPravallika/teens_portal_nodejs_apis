b208eea2a0da63c1cf93126d43e815a8fe7caec4
Pravallika <pravallikapaluri146@gmail.com>
2022-05-26 17:13:04 +0530

    added jobCategory and jobPost validation Files

--- routes/jobpost.js
+++ routes/jobpost.js
@@ -2,16 +2,8 @@ var express = require('express');
 var router = express.Router();
 const {jobpost_schema}=require('../validators')
 var cors = require ('cors')
-router.post("/signin",(req,res,next)=>{
-    try{
-        const{email,password}=req.body
-        const result= await register_schema.validateAsync(req.body)
-        console.log(result)
-    } 
-    catch(error){
-        if(error.Joi==true) error.status = 555
-        next(error)
-    }
-    res.send("signin")
+router.post("/jobpost",(req,res,next)=>{
+    router.post('/',cors(), jobpostController.create);
+    
 }
 )
